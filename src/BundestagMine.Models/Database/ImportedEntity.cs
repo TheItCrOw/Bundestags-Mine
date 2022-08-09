57ab8c2b22acd76446ba36086472b4d09de94d85
Kevin Bönisch <keboen@web.de>
2022-08-09 12:42:28 +0200

    Refactor the ImportedProtocol.cs to more general Entity Type.

---
+++ src/BundestagMine.Models/Database/ImportedEntity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BundestagMine.Models.Database
+{
+    public class ImportedEntity : DBEntity
+    {
+        public string ImportedDate { get; set; }
+
+        /// <summary>
+        /// This contains the protocol, agenda items, speeches and deputies.
+        /// </summary>
+        public string ModelJson { get; set; }
+
+        public ModelType Type { get; set; }
+
+        /// <summary>
+        /// If the model is of type NLPSpeech or deputy, then they belong to a protocol
+        /// </summary>
+        public Guid ProtocolId { get; set; }
+    }
+
+    public enum ModelType
+    {
+        Protocol,
+        NLPSpeech,
+        Deputy
+    }
+}
--- src/BundestagMine.SqlDatabase/Migrations/20220809104139_RefactorImportedProtocolToEntity.Designer.cs
+++ src/BundestagMine.SqlDatabase/Migrations/20220809104139_RefactorImportedProtocolToEntity.Designer.cs
@@ -4,14 +4,16 @@ using BundestagMine.SqlDatabase;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BundestagMine.SqlDatabase.Migrations
 {
     [DbContext(typeof(BundestagMineDbContext))]
-    partial class BundestagMineDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220809104139_RefactorImportedProtocolToEntity")]
+    partial class RefactorImportedProtocolToEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
